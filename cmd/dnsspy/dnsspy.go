6d1b72cf5dba61e86086fe35b87faca989c2d3d9
balhau <balhau@balhau.net>
2021-03-21 18:28:20 +0000

    Refactor dnsspy code

--- cmd/dnsspy/dnsspy.go
+++ cmd/dnsspy/dnsspy.go
@@ -1,136 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"errors"
-	"io/ioutil"
 	"log"
-	"net"
 	"os"
-	"os/exec"
-	"reflect"
-	"time"
-
-	"text/template"
 
 	"git.balhau.net/monitor/pkg/dns"
-	"github.com/go-co-op/gocron"
-	"gopkg.in/yaml.v2"
 )
 
 const (
 	DNS_UPDATER_FREQ = 20
 )
 
-var domains, erra = parseDomains()
-var globalips, errb = buildGlobalIPsMap(*domains)
-
 func main() {
-	s := gocron.NewScheduler(time.UTC)
-	s.Every(DNS_UPDATER_FREQ).Second().Do(updateDNS)
-	s.StartBlocking()
-}
+	var dnsSpy = dns.NewDnsBindSpy(os.Getenv(dns.DNS_CONFIG_PATH_ENV), DNS_UPDATER_FREQ)
 
-func testCron() {
-	log.Println("Task executed")
-	log.Println(globalips)
-	var updatedIps, _ = updatedIps(globalips)
-	log.Println(updatedIps)
-}
-
-func updatedIps(globalIps map[string][]string) (map[string][]string, error) {
-	var updatedIps = make(map[string][]string)
-	for name, _ := range globalIps {
-		addrs, err := net.LookupHost(name)
-		if err != nil {
-			return nil, err
-		}
-		updatedIps[name] = addrs
-	}
-	return updatedIps, nil
-}
-
-func buildGlobalIPsMap(domains dns.DNSBindConfig) (map[string][]string, error) {
-	log.Println(domains)
-	if &domains == nil {
-		return nil, errors.New("Invalid dns.DNSBindConfig")
-	}
-
-	var globalips = make(map[string][]string)
-
-	for _, domain := range domains.Domains {
-		globalips[domain.NameResolver] = domain.GlobalAddress
-	}
-
-	return globalips, nil
-}
-
-func parseDomains() (*dns.DNSBindConfig, error) {
-	yamlDNSFilePath := os.Getenv(dns.DNS_CONFIG_PATH_ENV)
-
-	yamlDNSFile, err := ioutil.ReadFile(yamlDNSFilePath)
-
+	var err = dnsSpy.InitSpy()
 	if err != nil {
 		log.Fatal(err)
-		return nil, err
 	}
-
-	var dnsConfigs dns.DNSBindConfig
-	err = yaml.Unmarshal(yamlDNSFile, &dnsConfigs)
-
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-
-	return &dnsConfigs, nil
-}
-
-func updateDNS() {
-
-	var updatedIps, _ = updatedIps(globalips)
-
-	if !reflect.DeepEqual(globalips, updatedIps) {
-		log.Println("Detected change of ips, updating dns entries", updatedIps)
-		for _, domain := range domains.Domains {
-			domainTemplate, err := ioutil.ReadFile(domain.TemplatePath)
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-
-			t, err := template.New(domain.Name).Parse(string(domainTemplate))
-
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-
-			buff := bytes.NewBufferString("")
-
-			domain.GlobalAddress = updatedIps[domain.NameResolver]
-
-			t.Execute(buff, domain)
-
-			f, err := os.Create(domain.OutputPath)
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-
-			f.Write(buff.Bytes())
-			f.Close()
-
-			cmd := exec.Command(domain.RestartCommand[0], domain.RestartCommand[1:]...)
-
-			err = cmd.Run()
-
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-		globalips = updatedIps
-	} else {
-		log.Println("No dns changes, skipping")
-	}
+	
+	dnsSpy.StartBlockingSpy()
 }
--- pkg/dns/bind.go
+++ pkg/dns/bind.go
@@ -1,5 +1,21 @@
 package dns
 
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"reflect"
+	"time"
+
+	"github.com/go-co-op/gocron"
+	"gopkg.in/yaml.v2"
+	"text/template"
+)
+
 const (
 	DNS_CONFIG_PATH_ENV string = "DNS_CONFIG_PATH_ENV"
 )
@@ -26,4 +42,133 @@ type Domain struct {
 //Dns Bind Template configurations.
 type DNSBindConfig struct {
 	Domains []Domain `yaml:"domains"`
+}
+
+type DnsBindSpy struct {
+	configYamlPath string
+	spyFrequency   int
+	dnsBindConfig  DNSBindConfig
+	globalIps      map[string][]string
+}
+
+func NewDnsBindSpy(configPath string, spyFreq int) DnsBindSpy {
+	return DnsBindSpy{
+		configYamlPath: configPath,
+		spyFrequency:   spyFreq,
+	}
+}
+
+func (spy DnsBindSpy) parseDomains() error {
+
+	yamlDNSFile, err := ioutil.ReadFile(spy.configYamlPath)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	var dnsConfigs DNSBindConfig
+
+	err = yaml.Unmarshal(yamlDNSFile, &dnsConfigs)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	spy.dnsBindConfig = dnsConfigs
+	return nil
+}
+
+func (spy DnsBindSpy) buildIpMap() error {
+	if &spy.dnsBindConfig == nil {
+		return errors.New("Invalid dns.DNSBindConfig")
+	}
+
+	spy.globalIps = make(map[string][]string)
+
+	for _, domain := range spy.dnsBindConfig.Domains {
+		spy.globalIps[domain.NameResolver] = domain.GlobalAddress
+	}
+
+	return nil
+}
+
+func (spy DnsBindSpy) updatedIps(globalIps map[string][]string) (map[string][]string, error) {
+	var updatedIps = make(map[string][]string)
+	for name, _ := range spy.globalIps {
+		addrs, err := net.LookupHost(name)
+		if err != nil {
+			return nil, err
+		}
+		updatedIps[name] = addrs
+	}
+	return updatedIps, nil
+}
+
+func (spy DnsBindSpy) InitSpy() error {
+	var err = spy.parseDomains()
+	if err != nil {
+		return err
+	}
+	err = spy.buildIpMap()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (spy DnsBindSpy) StartBlockingSpy() {
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(spy.spyFrequency).Second().Do(spy.updateDNS)
+	s.StartBlocking()
+}
+
+func (spy DnsBindSpy) updateDNS() {
+
+	var updatedIps, _ = spy.updatedIps(spy.globalIps)
+
+	if !reflect.DeepEqual(spy.globalIps, updatedIps) {
+		log.Println("Detected change of ips, updating dns entries", updatedIps)
+		for _, domain := range spy.dnsBindConfig.Domains {
+			domainTemplate, err := ioutil.ReadFile(domain.TemplatePath)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			t, err := template.New(domain.Name).Parse(string(domainTemplate))
+
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			buff := bytes.NewBufferString("")
+
+			domain.GlobalAddress = updatedIps[domain.NameResolver]
+
+			t.Execute(buff, domain)
+
+			f, err := os.Create(domain.OutputPath)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+
+			f.Write(buff.Bytes())
+			f.Close()
+
+			cmd := exec.Command(domain.RestartCommand[0], domain.RestartCommand[1:]...)
+
+			err = cmd.Run()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		spy.globalIps = updatedIps
+	} else {
+		log.Println("No dns changes, skipping")
+	}
 }
